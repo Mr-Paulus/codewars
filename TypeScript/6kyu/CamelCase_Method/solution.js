59f035ce1c8f5f6e8f953719b7a58efeaf7cdce1
Mr-Paulus <paul-van-eldonk@hotmail.com>
2023-08-13 19:43:10 +0200

    final CamelCase Method

---
+++ TypeScript/6kyu/CamelCase_Method/solution.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+exports.camelCase = void 0;
+function camelCase(str) {
+    //Empty strings cannot be CamelCase
+    if (str.length == 0) {
+        return str;
+    }
+    '';
+    str = str[0].toUpperCase() + str.slice(1).toLowerCase();
+    //remove spacing and make the next letter uppercase
+    while (str.indexOf(' ') > 0) {
+        str = str.slice(0, str.indexOf(' ')) + str[str.indexOf(' ') + 1].toUpperCase() + str.slice(str.indexOf(' ') + 2);
+    }
+    ;
+    return str;
+}
+exports.camelCase = camelCase;
+;
---
+++ TypeScript/6kyu/CamelCase_Method/solution.ts
@@ -0,0 +1,16 @@
+export function camelCase(str: string): string {
+    //Empty strings cannot be CamelCase
+    if (str.length == 0){
+        return str
+    }''
+
+
+    str = str[0].toUpperCase() + str.slice(1).toLowerCase();
+
+    //remove spacing and make the next letter uppercase
+    while(str.indexOf(' ') > 0){
+        str = str.slice(0,str.indexOf(' ')) + str[str.indexOf(' ')+1].toUpperCase() + str.slice(str.indexOf(' ')+2);
+    };
+
+    return str
+};
